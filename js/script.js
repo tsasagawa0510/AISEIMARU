c8dbf537985cc374d7fc7f4b1f04ef63306f65ad
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2023-12-14 16:24:27 +0900

    add

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ $('.js_slide').slick({
   autoplaySpeed: 5000,
   arrows: false,
 });
+
+$(window).scroll(function () {
+  const scrollAmount = $(window).scrollTop();
+  const windowHeight = $(window).height();
+  const adjust = windowHeight * 0.2;
+  $('.js-fadeIn').each(function () {
+    const position = $(this).offset().top;
+    if (scrollAmount > position - windowHeight + adjust) {
+      $(this).addClass('fade-in');
+    }
+  });
+});
