a110d528aaeb1ebf6cca2e38ddf10036c025dbaf
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2024-01-18 06:32:45 +0900

    add

--- js/script.js
+++ js/script.js
@@ -23,5 +23,78 @@ $(window).on("load", function() {
   $(".mainView_slide").attr("data-view", "true");
   $(".mainCatch_img").attr("data-view", "true");
   $(".mainCatch_p").attr("data-view", "true");
+  $(".headerMenu_btn").attr("data-view", "true");
   // console.log($(".mainView_slide").data("view"));
 });
+
+
+const modalBtn = document.querySelectorAll('.btn');
+const modalMask = document.querySelector('.modal-mask');
+const modal = document.querySelector('.modal-inner');
+const content = document.querySelector('.modal-text');
+const closeBtns = document.querySelectorAll('.close-modal');
+console.log(closeBtns);
+
+modalBtn.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    modalMask.classList.add('active');
+    modal.classList.add('active');
+    let inner = btn.getAttribute('data-model');
+    console.log(inner);
+    let target = document.querySelector(`p[data-model=${inner}]`);
+    content.textContent = target.textContent;
+  });
+});
+
+closeBtns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    modalMask.classList.remove('active');
+    modal.classList.remove('active');
+    content.textContent = "";
+    console.log('bbb');
+  });
+});
+
+function scrollCheck() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const element = document.querySelector('#top-btn');
+  const checkPoint = 250;
+
+  if(scrollTop > checkPoint){
+    element.style.opacity = 1;
+    element.style.visibility = "visible";
+  }
+}
+
+window.addEventListener('scroll', scrollCheck);
+
+$('a[href^="#"]').click(function () {
+  const speed = 700;
+  const href = $(this).attr('href');
+  let $target;
+  if (href == '#') {
+    $target = $('html');
+  }
+  else {
+    $target = $(href);
+  }
+  const position = $target.offset().top;
+  $('html, body').animate({ 'scrollTop': position }, speed, 'swing');
+  return false;
+});
+
+const menuBtn = document.querySelector('.headerMenu_btn');
+
+menuBtn.addEventListener('click', function() {
+  const nav = document.querySelector('.headerMenu_nav');
+  if(menuBtn.getAttribute('click-state') === "false" ){
+    menuBtn.setAttribute('click-state', true);
+    nav.style.opacity = 1;
+    nav.style.visibility = "visible";
+  }else{
+    menuBtn.setAttribute('click-state', "false");
+    nav.style.opacity = 0;
+    nav.style.visibility = "hidden";
+  }
+
+})
